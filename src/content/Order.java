4898f084c7a045f02c37840040e2af7e9ecfb16f
sherwinwater <sherwin.water@gmail.com>
2020-03-19 23:50:22 -0400

    init

---
+++ src/content/Order.java
@@ -0,0 +1,49 @@
+package content;
+
+public class Order {
+
+    private String orderID;
+    private String customerID;
+    private String product;
+    private String shippingMethod;
+    
+    public Order(String orderID){
+        this.orderID = orderID;
+    }
+
+    public String getOrderID() {
+        return orderID;
+    }
+
+    public void setOrderID(String orderID) {
+        this.orderID = orderID;
+    }
+
+    public String getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(String customerID) {
+        this.customerID = customerID;
+    }
+
+    public String getProduct() {
+        return product;
+    }
+
+    public void setProduct(String product) {
+        this.product = product;
+    }
+
+    public String getShippingMethod() {
+        return shippingMethod;
+    }
+
+    public void setShippingMethod(String shippingMethod) {
+        this.shippingMethod = shippingMethod;
+    }
+    
+    public String toString(){
+        return orderID+","+customerID+","+product+","+shippingMethod;
+    }
+}
